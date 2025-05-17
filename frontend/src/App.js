4bd964686576925af1343c0f8b8c479a20918274
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2025-05-17 17:17:09 +0530

    Normalize line endings

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// frontend/src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Government Scheme Recommender</h1>
+      <p>Start building your UI here.</p>
+    </div>
+  );
+}
+
+export default App;
